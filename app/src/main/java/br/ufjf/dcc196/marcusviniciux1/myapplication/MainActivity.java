b0a73aff571538e1dcd05ce55c67d251d571c848
marcusviniciux1 <marcusvinicius.rodrigues@estudante.ufjf.br>
2022-05-18 22:27:47 -0300

    adiciona evento ao botao

--- app/src/main/java/br/ufjf/dcc196/marcusviniciux1/myapplication/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/marcusviniciux1/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package br.ufjf.dcc196.marcusviniciux1.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clicaBotaoMensagem(View origem){
         TextView textViewMensagem = findViewById(R.id.textViewMensagem);
         textViewMensagem.setText("Olá mundo!");
     }
