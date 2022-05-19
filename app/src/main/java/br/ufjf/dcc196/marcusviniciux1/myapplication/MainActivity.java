57300962c34b9022ed98cb1e14aa0a3cd14f0e44
marcusviniciux1 <marcusvinicius.rodrigues@estudante.ufjf.br>
2022-05-18 22:41:20 -0300

    adiciona saida com base na entrada

--- app/src/main/java/br/ufjf/dcc196/marcusviniciux1/myapplication/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/marcusviniciux1/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +17,19 @@ public class MainActivity extends AppCompatActivity {
 
     public void clicaBotaoMensagem(View origem){
         TextView textViewMensagem = findViewById(R.id.textViewMensagem);
-        textViewMensagem.setText("Olá mundo!");
+        EditText editTextNome = findViewById(R.id.editTextTextNome);
+        EditText editTextStarter = findViewById(R.id.editTextTextStarter);
+
+        String nome = editTextNome.getText().toString();
+        String starter = editTextStarter.getText().toString();
+        String mensagem = String.format(
+                "Olá treinador %s! Seu starter é %s!",
+                nome,
+                starter
+        );
+
+
+
+        textViewMensagem.setText(mensagem);
     }
 }
